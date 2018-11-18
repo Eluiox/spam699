0928b8c62dccbe3e181529375e713f0a8a902cea
Eluiox <44433575+Eluiox@users.noreply.github.com>
2018-11-18 16:14:30 +0200

    Update bot (4).js

--- bot (4).js
+++ bot (4).js
@@ -1,16 +1,16 @@
 ﻿const Discord = require('discord.js');
 const client = new Discord.Client();
-var prefix = "1";
+var prefix = ".K";
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);  
 });
 
 client.on('ready', async() => {
-var server = "509357857666236430"; // ايدي السررفر
-var channel = "509357857666236434";//ايدي الروم
+var server = "509357857666236430"; // 504578013485334529
+var channel = "509357857666236434";//504578013938188294
     setInterval(()=>{
-    client.guilds.get(server).channels.get(channel).send('**Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , Dream , **')
+    client.guilds.get(server).channels.get(channel).send('**#Daily **')
     },305);
 })
 
